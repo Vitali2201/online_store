f828472067eaacac616689593b15dc6621ce551b
Vitali2201 <84346944+Vitali2201@users.noreply.github.com>
2021-09-20 00:05:27 +0300

    Add files via upload

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,7 +1,7 @@
 """ecommerce URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -21,6 +21,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
